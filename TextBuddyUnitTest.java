11c3faee96bf6031146f296f40a9f2413a0b78b7
JeanKoh <jkohx3@gmail.com>
2015-09-19 22:35:29 +0800

    starting on TDD for search

--- TextBuddyUnitTest.java
+++ TextBuddyUnitTest.java
@@ -4,30 +4,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyUnitTest {
-	
+
 	Logic logic;
-	
+
 	@Before
 	public void init(){
 		String[] args = new String[1];
 		args[0] = "test.txt";
 		logic = new Logic(args);
 	}
-	
+
 	@Test
 	public void testSort() {
 		//when contents is empty
 		logic.clear();
 		assertEquals("content is empty",logic.sortByAlpha());
-		
+
 		//adding contents
 		logic.add(" aaa");
 		assertEquals("1. aaa",logic.display().get(0));
-		
+
 		//checking first item in list
 		assertEquals("content is sorted", logic.sortByAlpha());
 		assertEquals("1. aaa", logic.display().get(0)); 
-		
+
 		//changing contents
 		logic.clear();
 		logic.add(" bbb");
@@ -36,6 +36,11 @@ public class TextBuddyUnitTest {
 		assertEquals("1. aaa", logic.display().get(0));
 		assertEquals("2. bbb", logic.display().get(1));
 
+	}
+
+	@Test
+	public void testSearch(){
+		fail("not implemented");
 	}
 
 }
